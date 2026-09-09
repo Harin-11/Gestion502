a738ed5237d5696b2f6234b0489cd507ad6249c0
Eshka <ezzie_git@proton.me>
2026-09-09 09:39:02 -0500

    feat(modulos): crear activities y layouts base para los 6 modulos del dashboard
    - Anade EmpresaActivity, GastosActivity, ListaTareaActivity, FavoritosActivity, MisDatosActivity y TareasActivity
    - Cada activity con layout basico (ConstraintLayout + EdgeToEdge)
    - Scaffolding para navegacion desde Dashboard

---
+++ app/src/main/java/com/madrigalsolu/gestion502/Lista_Tareas/ListaTareaActivity.java
@@ -0,0 +1,26 @@
+package com.madrigalsolu.gestion502.Lista_Tareas;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.madrigalsolu.gestion502.R;
+
+public class ListaTareaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_lista_tarea);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
